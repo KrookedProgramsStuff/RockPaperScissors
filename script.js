9df92357f7765de06a5fb10a615c564a14d1336b
KrookedProgramsStuff <132789908+KrookedProgramsStuff@users.noreply.github.com>
2023-07-26 21:40:37 -0700

    added score counter ad msg for score counter

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ return result
 
 let arr = ["rock", "paper", "scissors"]; /*Array for computer selection*/
 
+let score = 0; /*Score counter for game*/
+
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) { /* Runs the function 5 times*/
         playRound();
     }
+    alert("Game over! Your final score is: " + score + "\nRefresh the page to start over."); /*Alerts of game over after function has ran 5 times*/
 };
 
 
@@ -23,47 +26,77 @@ function playRound() { /*Function that plays game*/
 
     playerSelection = playerSelection.toLowerCase(); /*Converts selection to lowercase*/
     if (playerSelection == computerSelection){ /*Compares player and computer selection*/
-        alert("You and the computer are thinking alike, play again!");
-        console.log("You and the computer are thinking alike, play again!");
+        alert("You and the computer are thinking alike, play again!"  + "Your score is: " + score);
+        console.log("You and the computer are thinking alike, play again! "  + "Your score is: "  + score);
     }
     else if (playerSelection == "rock" && computerSelection == "paper"){
-        alert("The computer chose paper, you lose!");
-        console.log("The computer chose paper, you lose!");
+
+        if (score <= 0) { /*Checks the score value, if it's 0 or less than, it just returns its value*/
+            return score;
+        }
+        else {
+            score--; /*Reduces the score by 1*/
+        }
+        alert("The computer chose paper, you lose! " + "Your score is: " + score);
+        
+        console.log("The computer chose paper, you lose!" + "Your score is: "  + score);
 
     }
     else if (playerSelection == "rock" && computerSelection == "scissors"){
-        alert("The computer chose scissors, you win!");
-        console.log("The computer chose scissors, you win!");
+
+        score++; /*Increases the score by 1*/
+        alert("The computer chose scissors, you win! " + "Your score is: "  + score);
+        
+        console.log("The computer chose scissors, you win! " + "Your score is: "  + score);
 
     }
     else if (playerSelection == "scissors" && computerSelection == "rock"){
-        alert("The computer chose rock, you lose!");
-        console.log("The computer chose rock, you lose!");
+
+        if (score <= 0) {
+            return score;
+        }
+        else {
+            score--;
+        }
+        alert("The computer chose rock, you lose! " + "Your score is: "  + score);
+       
+        console.log("The computer chose rock, you lose! " + "Your score is: "  + score);
 
     }
     else if (playerSelection == "scissors" && computerSelection == "paper"){
-        alert("The computer chose paper, you win!");
-        console.log("The computer chose paper, you win!");
+
+        score++;
+        alert("The computer chose paper, you win!" + "Your score is: "  + score);
+        
+        console.log("The computer chose paper, you win! " + "Your score is: "  + score);
 
     }
     else if (playerSelection == "paper" && computerSelection == "scissors"){
-        alert("The computer chose scissors, you lose!");
-        console.log("The computer chose scissors, you lose!");
+        if (score <= 0) {
+            return score;
+        }
+        else {
+            score--;
+        }
+        
+        alert("The computer chose scissors, you lose! " + "Your score is: "  + score);
+        
+        console.log("The computer chose scissors, you lose! " + "Your score is: "  + score);
 
     }
     else if (playerSelection == "paper" && computerSelection == "rock"){
-        alert("The computer chose rock, you win!");
-        console.log("The computer chose rock, you win!");
+        score++;
+        alert("The computer chose rock, you win! " + "Your score is: "  + score);
+        console.log("The computer chose rock, you win! " + "Your score is: "  + score);
 
     }
     
 };
 
-/* From here make variable that checks score and returns it, then prints final score at the end of the game */
-game();
+game(); /*Runs the game function*/
 
 
 
-function getComputerChoice() {
+function getComputerChoice() { /*Allows the computer to randomly select rock, paper, scissors */
     return arr[(Math.floor(Math.random() * arr.length))];
 }
